4f2c82271066e0e6d8029491aa3eb4e656b01a7b
Rade Radovanović <rade.radovanovic@skyline.be>
2023-07-18 12:35:52 +0200

    Adding scripts to MediaOps folder.

--- DeleteRoute_1/DeleteRoute_1.cs
+++ DeleteRoute_1/DeleteRoute_1.cs
@@ -45,7 +45,7 @@ Revision History:
 
 DATE		VERSION		AUTHOR			COMMENTS
 
-dd/mm/2023	1.0.0.1		XXX, Skyline	Initial version
+dd/mm/2023	1.0.0.1		RRA, Skyline	Initial version
 ****************************************************************************
 */
 
